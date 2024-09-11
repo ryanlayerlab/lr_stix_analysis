7dd250363c172a4ef070b6a7d00e9e9d42d426ed
Ryan Layer <ryan.layer@gmail.com>
2024-09-11 12:09:47 -0600

    add 1kg lr result

--- README.md
+++ README.md
@@ -25,3 +25,24 @@ cat data.data/lr_te_pop_freq_t_5.bed \
 <Figure>
 ![lr_te_freq](img/lr_te_pop_freq_t_5.hist.png)
 </Figure>
+
+
+```
+python src/get_pop_freq.py \
+    --t 5 \
+    --lr "data/LR_STIX_1kg_queries/04.3.SR_all.slop100.tmp*.results
+> data/lr_1kg_pop_freq_t_5.bed
+
+cat data/lr_1kg_pop_freq_t_5.bed \
+| cut -f 4 \
+| python src/hist.py \
+    --out_file img/lr_1kg_pop_freq_t_5.hist.png \
+    --log \
+    --xlabel "Pop Freq."\
+    --ylabel "Freq."
+```
+<Figure>
+![lr_te_freq](img/lr_1kg_pop_freq_t_5.hist.png)
+</Figure>
+
+
--- src/get_pop_freq.py
+++ src/get_pop_freq.py
@@ -1,8 +1,9 @@
 import argparse
+import glob
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--lr', type=str, help='Long read file', required=True)
+    parser.add_argument('--lr', type=str, help='Long read file or files')
     parser.add_argument('--t',
                         type=int,
                         help='Depth threshold (default = 1)',
@@ -10,31 +11,36 @@ def parse_args():
                         required=True)
     return parser.parse_args()
 
+def parse_lr_file(lr_file, LR, t):
+    with open(lr_file, 'r') as f:
+        sv = None
+        for line in f:
+            if line.startswith('>>>'):
+                l,r = line[3:].rstrip().split()[:2]
+                lchr,lpos = l.split(':')
+                rchr,rpos = r.split(':')
+                lstart,lend= lpos.split('-')
+                rstart,rend= rpos.split('-')
+                sv = (lchr,lstart,rstart)
+                if sv not in LR:
+                    LR[sv] = 0
+                depth = 0
+                ditch_line = f.readline()
+                while not ditch_line.startswith('Giggle_File_Id'):
+                    ditch_line = f.readline()
+            else:
+                paired,split = [int(x) for x in line.rstrip().split()[-2:]]
+                LR[sv] = LR[sv] + (paired + split >= t )
+    return LR
+
 def main():
     args = parse_args()
-
 
     if args.lr is not None:
         LR = {}
 
-        with open(args.lr, 'r') as f:
-            sv = None
-            for line in f:
-                if line.startswith('>>>'):
-                    l,r = line[3:].rstrip().split()[:2]
-                    lchr,lpos = l.split(':')
-                    rchr,rpos = r.split(':')
-                    lstart,lend= lpos.split('-')
-                    rstart,rend= rpos.split('-')
-                    sv = (lchr,lstart,rstart)
-                    LR[sv] = 0
-                    depth = 0
-                    #read off the next 3 lines
-                    for i in range(3):
-                        f.readline()
-                else:
-                    paired,split = [int(x) for x in line.rstrip().split()[-2:]]
-                    LR[sv] = LR[sv] + (paired + split >= args.t )
+        for file in glob.glob(args.lr):
+            LR = parse_lr_file(file, LR, args.t)
 
         for sv in LR:
             print('\t'.join(list(sv) + [str(LR[sv])]))
