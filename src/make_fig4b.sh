8e93d60221d0bf52711734266fa065251f647e6b
zhengxinchang <zhengxinchang1993@163.com>
2024-09-25 13:50:08 -0500

    update fig4B ylabel

--- src/make_fig4b.sh
+++ src/make_fig4b.sh
@@ -4,8 +4,8 @@ cat data/lr_cosmic_pop_freq_t_5.bed \
     --bins 0 0.001 .01 .05 1.0 \
     --bin_names "0%" "(0%-1%]" "(1%-5%]" "(5%-100%]" \
     --out_file img/lr_cosmic_freq_fixed_bins_t_5.png \
-    --xlabel "% of samples with long-read depth > 5"\
-    --ylabel "Number of TEs" \
+    --xlabel "% of samples with long-read depth >= 5"\
+    --ylabel "Number of COSMIC SVs" \
     --title "COSMIC SVs" \
     --height 4 \
     --width 5 \
